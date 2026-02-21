b09d55ed3ef03b503daca49eff5465f541e40761
Chaewon <codenamewont@gmail.com>
2026-02-22 03:42:31 +0900

    chore: 템플릿 코드 제거 및 초기 앱 구조 정리

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-xl font-bold">GIFTEE</Text>
+      </View>
       <StatusBar style="auto" />
     </SafeAreaProvider>
   );
