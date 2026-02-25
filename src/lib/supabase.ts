851e6e5efc7a2a2b5acc5d92e941905ae557a9c1
Chaewon <codenamewont@gmail.com>
2026-02-25 23:34:38 +0900

    feat: 구글 로그인 인증 구조 설정 및 검증 완료

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,15 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://rtosgywwrsihydewuppq.supabase.co';
 const supabaseKey = 'sb_publishable_MbmTJui82PQWqenSSIR54w_SJ3WLulW';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: AsyncStorage,
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: false,
+    flowType: 'pkce',
+  },
+});
